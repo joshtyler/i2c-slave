cd7f7bf7a0f1f803b09ed722de895fa4f93bdc5a
joshtyler <josh.tyler@btinternet.com>
2016-10-03 13:50:26 +0100

    All reconfigured. Pre change debouncer before testing

--- hdl/i2c.vhd
+++ hdl/i2c.vhd
@@ -7,15 +7,33 @@ use work.i2c_package.all;
 entity i2c is
     Generic ( no_ro_regs : integer range 0 to 128; --Address space is 0x0 to 0x7F. 128 combinations
 	           no_rw_regs : integer range 0 to 128; --Address space is 0x80 to 0xFF. 128 combinations
-	           module_addr : STD_LOGIC_VECTOR(SLV_ADDR_WIDTH-1 downto 0));
-    Port ( sda : in  STD_LOGIC;
+	           module_addr : STD_LOGIC_VECTOR(SLV_ADDR_WIDTH-1 downto 0);
+				  sample_clock_frequency : integer;
+				  i2c_settling_time : time);
+    Port ( clk : in STD_LOGIC;
+           sda : in  STD_LOGIC;
            sda_wen : out STD_LOGIC;
            scl : in  STD_LOGIC;
            ro_regs : in  array8 (0 to no_ro_regs-1);
            rw_regs : out  array8 (0 to no_rw_regs-1));
 end i2c;
 
-architecture Behavioral of i2c is
+architecture Mixed of i2c is
+
+	component debounce is
+		Generic ( clock_frequency : integer; --Clock frequency in Hertz
+					 settling_time : time); --Maximum signal settling time
+		Port ( clk : in std_logic;
+				 input : in std_logic;
+				 output : out std_logic;
+				 change : out std_logic);
+	end component;
+	
+	-- Debouncer signals
+	signal sda_dbncd : std_logic;
+	signal scl_dbncd : std_logic;
+	signal sda_change : std_logic;
+	signal scl_change : std_logic;
 	
 	-- Top level state machine definition
 	type sm_type is (SM_RECEIVE, SM_SEND, SM_SEND_ACK, SM_RECEIVE_ACK, SM_WAIT);
@@ -24,6 +42,11 @@ architecture Behavioral of i2c is
 	-- Receive state machine definition
 	type rec_sm_type is (REC_SM_SLV_ADDR, REC_SM_REG_ADDR, REC_SM_DATA);
 	signal receive_state : rec_sm_type;
+	
+	-- Module status
+	type status_type is (ST_STOPPED, ST_STARTED, ST_REP_STARTED);
+	signal status : status_type := SM_STOPPED;
+	signal status_change : std_logic := '1';
 	
 	--State machine signals
 	type mode_type is (MD_RECEIVE, MD_SEND);
@@ -48,171 +71,184 @@ begin
 	--Intermediatory signals to allow readback of output
 	sda_wen <= sda_wen_buf;
 	rw_regs <= rw_regs_buf;
-
-	-- Detect start condition. Reset on first rising edge of SCL.
-	detect_start : process(sda, start_reset)
-	begin
-		if start_reset = '1' then
-			start_condition <= '0';
-		elsif falling_edge(sda) then
-			if scl = '1' then
-				start_condition <= '1';
-			end if;
-		end if;
-	end process detect_start;
 	
-	reset_start : process(scl)
-	begin
-		if rising_edge(scl) then
-			if start_condition = '1' then
-				start_reset <= '1';
-			else
-				start_reset <= '0';
-			end if;
-		end if;
-	end process reset_start;
 	
-	-- Detect stop condition. Reset on first rising edge of SCL.
-	detect_stop : process(sda, stop_reset)
-	begin
-		if stop_reset = '1' then
-			stop_condition <= '0';
-		elsif rising_edge(sda) then
-			if scl = '1'  then
-				stop_condition <= '1';
-			end if;
-		end if;
-	end process detect_stop;
-	
-	reset_stop : process(scl)
-	begin
-		if rising_edge(scl) then
-			if stop_condition = '1' then
-				stop_reset <= '1';
-			else
-				stop_reset <= '0';
-			end if;
-		end if;
-	end process reset_stop;	
-
-
-	main_sm : process(scl, start_condition, stop_condition)
-	begin
-		--Reset if start condition is 1
-		if start_condition = '1' then
-			-- Reset
-			top_state <= SM_RECEIVE;
-			receive_state <= REC_SM_SLV_ADDR;
-			receive_ctr <= 0;
-		end if;
+	--SDA debouncer
+	sda_debounce : debounce
+	generic map (
+		clock_frequency => sample_clock_frequency,
+		settling_time => i2c_settling_time
+		)
+	port map (
+		clk => clk,
+		input => sda,
+		output => sda_dbncd,
+		change => sda_change
+		);
 		
-		-- Run main state machine when not stopped
-		-- stop_condition = '1' and start_condition = '1' is the exception because this means that this scl edge is the first edge of a new transaction
-		-- Note the logic here could be expressed more compactly but is left verbose for clarity
-		if rising_edge(scl) and (stop_condition = '0' or (stop_condition = '1' and start_condition = '1')) then
-			case top_state is
-				--Receive module address
-				when SM_RECEIVE =>
-					--If we have received the entire word
-					if receive_ctr = D_WIDTH-1 then
-						top_state <= SM_SEND_ACK; -- May be overwritten later
-						receive_ctr <= 0;
-						case receive_state is
-							when REC_SM_SLV_ADDR =>
-								receive_state <= REC_SM_REG_ADDR;
-								-- If the slave address matches
-								if data(D_WIDTH-2 downto 0) = module_addr then
-									-- Save whether read or write mode
-									if sda = '1' then
-										mode <= MD_SEND;
-										if not(check_in_range(reg_addr, no_rw_regs, RW_REG_OFFSET) or check_in_range(reg_addr, no_ro_regs, RO_REG_OFFSET)) then
-											--If the requested register is out of range
-											top_state <= SM_WAIT;
+	--SCL debouncer
+	scl_debounce : debounce
+	generic map (
+		clock_frequency => sample_clock_frequency,
+		settling_time => i2c_settling_time
+		)
+	port map (
+		clk => clk,
+		input => scl,
+		output => scl_dbncd,
+		change => scl_change
+		);
+
+	-- Detect start stop status
+	sys_status : process(clk)
+	begin
+		if rising_edge(clk) then
+			status_change <= '0';
+			-- On SDA edge when SCL is 1
+			if sda_change = '1' and scl_dbncd = '1' then
+				status_change <= '1';
+				-- Start condition - falling sda edge
+				if sda_dbncd = '0' then
+					case status is
+						when ST_STOPPED =>
+							status <= ST_STARTED;
+						when ST_STARTED | ST_REP_STARTED =>
+							status <= ST_REP_STARTED;
+						when others =>
+							--Should never happen
+							status <= ST_STOPPED;
+					end case;
+				else
+				-- Stop condition - rising sda edge
+					status <= ST_STOPPED;
+				end if;
+			end if;
+		end if;
+	
+	end process sys_status;
+
+	-- Receive stat machine
+	main_sm : process(clk)
+	begin
+	
+		if rising_edge(clk) then	
+			--Reset on change of status
+			if status_change = '1' then
+				-- Reset
+				top_state <= SM_RECEIVE;
+				receive_state <= REC_SM_SLV_ADDR;
+				receive_ctr <= 0;
+			end if;
+			
+			-- Run main state machine when not stopped and we get a rising scl edge
+			if scl_change = '1' and scl_dbncd = '1' and status /= ST_STOPPED then
+				case top_state is
+					--Receive module address
+					when SM_RECEIVE =>
+						--If we have received the entire word
+						if receive_ctr = D_WIDTH-1 then
+							top_state <= SM_SEND_ACK; -- May be overwritten later
+							receive_ctr <= 0;
+							case receive_state is
+								when REC_SM_SLV_ADDR =>
+									receive_state <= REC_SM_REG_ADDR;
+									-- If the slave address matches
+									if data(D_WIDTH-2 downto 0) = module_addr then
+										-- Save whether read or write mode
+										if sda_dbncd = '1' then
+											mode <= MD_SEND;
+											if not(check_in_range(reg_addr, no_rw_regs, RW_REG_OFFSET) or check_in_range(reg_addr, no_ro_regs, RO_REG_OFFSET)) then
+												--If the requested register is out of range
+												top_state <= SM_WAIT;
+											end if;
+										else
+											mode <= MD_RECEIVE;
 										end if;
 									else
-										mode <= MD_RECEIVE;
+										top_state <= SM_WAIT;
 									end if;
-								else
-									top_state <= SM_WAIT;
-								end if;
-							when REC_SM_REG_ADDR =>
-								reg_addr <= data(D_WIDTH-2 downto 0) & sda;
-								receive_state <= REC_SM_DATA;
-							when REC_SM_DATA =>
-								if check_in_range(reg_addr, no_rw_regs, RW_REG_OFFSET) then
-									rw_regs_buf(to_integer(unsigned(reg_addr))) <= data(D_WIDTH-2 downto 0) & sda;
-									reg_addr <= std_logic_vector(unsigned(reg_addr) + 1);
-								else
-									top_state <= SM_WAIT; -- Out of range, so do not ack and wait for next transaction
-								end if;
-						end case;
-					else
-						-- Shift left, as MSB first
-						data <= data(D_WIDTH-2 downto 0) & sda;
-						receive_ctr <= receive_ctr + 1;
-					end if;
-					
-				when SM_SEND_ACK =>
-					--Sent in external process
-					if mode = MD_RECEIVE then
-						top_state <= SM_RECEIVE;
-					else
-						top_state <= SM_SEND;
-					end if;
-				
-				when SM_SEND =>
-					if send_ctr = 1 then
-						--We've transmitted the entire word, check for ack
-						if sda = '0' and (check_in_range(std_logic_vector(unsigned(reg_addr) + 1), no_rw_regs, RW_REG_OFFSET) or check_in_range(std_logic_vector(unsigned(reg_addr) + 1), no_ro_regs, RO_REG_OFFSET)) then
-							--Master ACK and next register would be in range
-							reg_addr <= std_logic_vector(unsigned(reg_addr) + 1);
+								when REC_SM_REG_ADDR =>
+									reg_addr <= data(D_WIDTH-2 downto 0) & sda_dbncd;
+									receive_state <= REC_SM_DATA;
+								when REC_SM_DATA =>
+									if check_in_range(reg_addr, no_rw_regs, RW_REG_OFFSET) then
+										rw_regs_buf(to_integer(unsigned(reg_addr))) <= data(D_WIDTH-2 downto 0) & sda_dbncd;
+										reg_addr <= std_logic_vector(unsigned(reg_addr) + 1);
+									else
+										top_state <= SM_WAIT; -- Out of range, so do not ack and wait for next transaction
+									end if;
+							end case;
 						else
-							--Master NACK or next register would not be in range
-							top_state <= SM_WAIT;
+							-- Shift left, as MSB first
+							data <= data(D_WIDTH-2 downto 0) & sda_dbncd;
+							receive_ctr <= receive_ctr + 1;
 						end if;
-					end if;
+						
+					when SM_SEND_ACK =>
+						--Sent in external process
+						if mode = MD_RECEIVE then
+							top_state <= SM_RECEIVE;
+						else
+							top_state <= SM_SEND;
+						end if;
 					
-				when SM_WAIT=>
-					-- Do nothing. We will reset on next start condition
-				when others =>
-					-- This should never happen!
-					-- Do nothing
-			end case;
-			
+					when SM_SEND =>
+						if send_ctr = 1 then
+							--We've transmitted the entire word, check for ack
+							if sda_dbncd = '0' and (check_in_range(std_logic_vector(unsigned(reg_addr) + 1), no_rw_regs, RW_REG_OFFSET) or check_in_range(std_logic_vector(unsigned(reg_addr) + 1), no_ro_regs, RO_REG_OFFSET)) then
+								--Master ACK and next register would be in range
+								reg_addr <= std_logic_vector(unsigned(reg_addr) + 1);
+							else
+								--Master NACK or next register would not be in range
+								top_state <= SM_WAIT;
+							end if;
+						end if;
+						
+					when SM_WAIT=>
+						-- Do nothing. We will reset on next start condition
+					when others =>
+						-- This should never happen!
+						-- Do nothing
+				end case;
+				
+			end if;
 		end if;
 	end process main_sm;
 	
-	--Send data on falling edge.
-	process(scl)
+	--Send state machine
+	process(clk)
 		variable current_data : std_logic_vector(D_WIDTH-1 downto 0);
 	begin
-		if falling_edge(scl) then
-			case top_state is
-				when SM_SEND_ACK =>
-					sda_wen_buf <= '1';
-					
-				when SM_SEND =>
-						if send_ctr = D_WIDTH+1 then
-							sda_wen_buf <= '0'; -- Stop transmit for ACK
-							send_ctr <= 1; --Set to 1 to make indexing nice
-						else
-							--Check if we are in the read only portion, or the read/write portion of memory
-							if check_in_range(reg_addr, no_rw_regs, RW_REG_OFFSET) then
-								current_data := rw_regs_buf(to_integer(unsigned(reg_addr(D_WIDTH-2 downto 0))));
+		if rising_edge(clk) then
+			-- Run on falling SCL edge
+			if scl_change = '1' and scl_dbncd = '0' and status /= ST_STOPPED then
+				case top_state is
+					when SM_SEND_ACK =>
+						sda_wen_buf <= '1';
+						
+					when SM_SEND =>
+							if send_ctr = D_WIDTH+1 then
+								sda_wen_buf <= '0'; -- Stop transmit for ACK
+								send_ctr <= 1; --Set to 1 to make indexing nice
 							else
-								current_data := ro_regs(to_integer(unsigned(reg_addr(D_WIDTH-2 downto 0))));
+								--Check if we are in the read only portion, or the read/write portion of memory
+								if check_in_range(reg_addr, no_rw_regs, RW_REG_OFFSET) then
+									current_data := rw_regs_buf(to_integer(unsigned(reg_addr(D_WIDTH-2 downto 0))));
+								else
+									current_data := ro_regs(to_integer(unsigned(reg_addr(D_WIDTH-2 downto 0))));
+								end if;
+								--Send data MSB first
+								sda_wen_buf <= not current_data(D_WIDTH-send_ctr);
+								send_ctr <= send_ctr + 1;
 							end if;
-							--Send data MSB first
-							sda_wen_buf <= not current_data(D_WIDTH-send_ctr);
-							send_ctr <= send_ctr + 1;
-						end if;
-					
-				when others =>
-					sda_wen_buf <= '0';
-			end case;
-		end if;		
+						
+					when others =>
+						sda_wen_buf <= '0';
+				end case;
+			end if;
+		end if;
 	end process;
 
 	
-end Behavioral;
+end Mixed;
 
